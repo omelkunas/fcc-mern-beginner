00b244a9a37c1942ad507a3a1d4df8a48e3b8fe9
Oleg Melkunas <o.melkunas@gmail.com>
2024-12-17 11:39:41 +0400

    protuct router and controller are created

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 
+import productRoutes from "./routes/product.route.js";
+
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
+app.use(express.json()); // allows to accept JSON
 
-app.listen(5000, () => {
+app.use("/api/products", productRoutes);
+
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://localhost:5000/");
+  console.log("Server started at http://localhost:" + PORT);
 });
