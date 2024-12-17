2ec7c05464cc4611471379dfbff964dbc34ff999
Oleg Melkunas <o.melkunas@gmail.com>
2024-12-17 22:14:38 +0400

    add Navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, useColorModeValue } from "@chakra-ui/react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
@@ -6,7 +6,7 @@ import CreatePage from "./pages/CreatePage";
 
 function App() {
   return (
-    <Box minH={"100vh"}>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,48 @@
-import React from "react";
+import {
+  Container,
+  Flex,
+  Text,
+  HStack,
+  Button,
+  useColorMode,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { PlusSquareIcon, StarIcon, SunIcon } from "@chakra-ui/icons";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row",
+        }}
+      >
+        <Text
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight="bold"
+          textTransform={"uppercase"}
+          bgGradient="linear(to-r, cyan.400, blue.500)"
+          bgClip="text"
+        >
+          <Link to={"/"}>Product Store 🛒</Link>
+        </Text>
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to={"/create"}>
+            <Button>
+              <PlusSquareIcon fontSize={20} />
+            </Button>
+          </Link>
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <StarIcon /> : <SunIcon />}
+          </Button>
+        </HStack>
+      </Flex>
+    </Container>
+  );
 }
